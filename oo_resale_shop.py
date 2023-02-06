aeb9c3184fe29e7780c16f0c008fbbae72910400
mneedham01 <mneedham@smith.edu>
2023-02-06 15:47:04 -0500

    after office hours

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,28 @@
+from computer import * 
+
 class ResaleShop:
 
     # What attributes will it need?
+    inventory: list
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self):
+        self.inventory=[]
 
     # What methods will you need?
+    def buy(self,description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price)-> None:
+        new_computer=Computer(description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price)
+        self.inventory.append(new_computer)
+    
+    def sell(self, c:Computer)- >None:
+        if c in self.inventory:
+            inventory.remove(c)
+        else:
+            print("Computer not found.")
+
+def main():
+    myStore= ResaleShop()
+    myStore.buy("a","a",1,2, "a",3,5)
+    print(myStore.inventory)
+    myStore.sell(myStore.inventory[0])
