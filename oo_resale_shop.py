c4159c52d429480b607204106fc3d4a63c24d98b
mneedham01 <mneedham@smith.edu>
2023-02-07 11:49:35 -0500

    adding unfinished refurbish code

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -30,7 +30,32 @@ class ResaleShop:
         else:
             print("Empty inventory.")
 
-    #def update_price()
+    def refurbish(self,c:Computer) -> None:
+        if c in self.inventory:
+            if c.
+
+
+    def refurbish(item_id: int, new_os: Optional[str] = None):
+        if item_id in self.inventory:
+            computer =  self.inventory[item_id] # locate the computer
+            if int(computer["year_made"]) < 2000:
+                computer["price"] = 0 # too old to sell, donation only
+            elif int(computer["year_made"]) < 2012:
+                computer["price"] = 250 # heavily-discounted price on machines 10+ years old
+            elif int(computer["year_made"]) < 2018:
+                computer["price"] = 550 # discounted price on machines 4-to-10 year old machines
+            else:
+                computer["price"] = 1000 # recent stuff
+
+            if new_os is not None:
+                computer["operating_system"] = new_os # update details after installing new OS
+        else:
+            print("Item", item_id, "not found. Please select another item to refurbish.")
+    new_OS = "MacOS Monterey"
+    print("Refurbishing Item ID:", computer_id, ", updating OS to", new_OS)
+    print("Updating inventory...")
+    refurbish(computer_id, new_OS)
+    print("Done.\n")
 
 
 
