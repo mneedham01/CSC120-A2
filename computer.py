a423f5f14ce0e29a10a1aedff0cc9f70e58691cd
mneedham01 <mneedham@smith.edu>
2023-02-07 16:25:53 -0500

    updated the price sign

--- computer.py
+++ computer.py
@@ -39,4 +39,4 @@ class Computer:
         print("Memory:",self.memory,"GB")
         print("Operating System:",self.operating_system)
         print("Year Made",self.year_made)
-        print("Price:", self.price)
+        print("Price: $", self.price)
